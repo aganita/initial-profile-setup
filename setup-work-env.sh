3a5fb9dd608f31455d750753d0f07c5b2bba1b14
Ani Agajanyan <aganita21@gmail.com>
2017-02-15 12:43:58 -0500

    cleaned up the script from useless comment

--- setup-work-env.sh
+++ setup-work-env.sh
@@ -10,55 +10,30 @@ dir_path=${dir_path:-"~/workspace"}
 # refactor user's' input to expand tilde. !!!NEVER eval user's input!!!
 dir_path=${dir_path/#\~/$HOME}
 
-# create the main working directory and directory for log files
 mkdir -p $dir_path
 mkdir -p $dir_path/my-error-log
 
-# check if the directory is created
 if [[ -d "$dir_path" ]]; then
   echo "YAY 1! Your directory is ready: $dir_path"
 fi
 
-# check for node and install stable version
 brew install -g n
 
-# create and clone all of the respective repos
 git clone https://github.com/aganita/initial-profile-setup.git $dir_path/initial-profile-setup
   echo "YAY 2! Your profile files are cloned to: $dir_path"
-
-# install node version control and curl
+  
 npm install -g n
 brew install curl
 
-# for bash
 ln -sf  $dir_path/initial-profile-setup/bash/.bash_profile $HOME/.bash_profile
 
-# install zsh
-brew install zsh
-
 # install and setup zsh themes oh-my-zsh
+brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -sf $dir_path/initial-profile-setup/zshell/.zshrc $HOME/.zshrc
 
-# create a symlink for my custom aliases
 ln -sf $dir_path/initial-profile-setup/.my_aliases $HOME/.my_aliases
-
-# create a separate file for all env variables
 touch $HOME/.set_my_exports
 
-# install nodemon and webpack
 npm install -g nodemon
 npm install -g webpack
-
-# write node_env to shell profile
-shellProfile='unknown'
-if [[ $SHELL == "/bin/zsh" ]]; then
-  echo "zsh"
-  shellProfile=~/.zshrc
-elif [[ $SHELL == "/bin/bash" ]]; then
-  echo "bash"
-  shellProfile=~/.bash_profile
-fi
-
-echo export NODE_ENV="development" >> $shellProfile
-source $shellProfile
