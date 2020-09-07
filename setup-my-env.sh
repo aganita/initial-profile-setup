f66f16543c53b13558515ab5f8a664659e2f4ae4
aganita <aganita21@gmail.com>
2020-09-06 22:23:11 -0700

    add settings

--- setup-my-env.sh
+++ setup-my-env.sh
@@ -4,9 +4,10 @@ set -e # stop if error
 #set -x # verbose installation
 
 GREEN=`tput setaf 2` # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+RESET_COLOR=`tput sgr0`
 
-echo "Enter your working directory. Hit Enter for default setting (~/workspace)"
-read dir_path
+# echo "Enter your working directory. Hit Enter for default setting (~/workspace)"
+# read dir_path
 dir_path=${dir_path:-"~/workspace"}
 
 dir_path=${dir_path/#\~/$HOME}
@@ -24,14 +25,14 @@ ln -sf $dir_path/my-profile/settings/.my_exports $HOME/.my_exports
 brew install zsh
 ln -sf $dir_path/my-profile/zshell/.zshrc $HOME/.zshrc
 
+brew cask install vscodium
+rm $HOME/Library/Application\ Support/VSCodium/User/settings.json || true
+ln -sf $dir_path/my-profile/settings/vsc-settings.json $HOME/Library/Application\ Support/VSCodium/User/settings.json
+
 read -p "${GREEN}====> Rebind Caps Lock key to Ctrl and Enter to continue"
 
 read -p "====> Install iTerm2. Press Enter to continue"
 ln -sf $dir_path/my-profile/settings/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
-
-read -p "====> Install VSCodium. Press Enter to continue"
-rm $HOME/Library/Application\ Support/VSCodium/User/settings.json
-ln -sf $dir_path/my-profile/settings/vsc-settings.json $HOME/Library/Application\ Support/VSCodium/User/settings.json
 
 read -p "====> Install font Meslo LG M DZ Regular for Powerline located at my-profile/settings. Press Enter to continue"
 
@@ -40,7 +41,7 @@ read -p "====> Install NodeJS browser. Press Enter to continue"
 read -p "====> Install Brave browser. Press Enter to continue"
 read -p "Copy over bookmarks for Brave. Press Enter to continue"
 
-read -p "====> Install Chrome browser. Press Enter to continue"
+read -p "====> Install Chrome browser. Press Enter to continue${RESET_COLOR}"
 
  
 
